80df69eac74649740c20c3d06ea431197c7efef4
EliDerDeli <EliDerDeli@gmail.com>
2021-01-06 15:48:59 +0100

    Test 2

--- scrapper.R
+++ scrapper.R
@@ -1,1 +1,19 @@
 #webscrapper
+
+library(tidyverse)
+library(rvest)
+library(rebus)
+library(lubridate)
+library(stringr)
+library(plyr)
+library(dplyr)
+library(ggvis)
+library(knitr)
+
+#a hlinks 
+
+url <-'https://www.nasdaq.com/market-activity/ipos'
+
+stock <- read_html(url) 
+stock.list <- html_nodes(stock, '.market-calendar-table--with-title')
+stocks.names<-html_text(stock.list)
